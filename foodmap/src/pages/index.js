4d8066bf061468cec1af65d5b23b31b1fd3891a0
YongKi <p.ryan9311@gmail.com>
2023-05-29 17:19:24 +0900

    로그인 페이지 1차 작업

--- foodmap/src/pages/index.js
+++ foodmap/src/pages/index.js
@@ -1,25 +1,28 @@
+import styled from "@emotion/styled";
 import Image from "next/image";
 
 const HomeLoginPage = () => {
   return (
-    <>
+    <Logo>
       <div>
         <Image src={"/Logo.png"} alt={"Logo image"} width={400} height={400} />
       </div>
       <div>
         <input type="text" placeholder="id를 입력해주세요" />
-      </div>
-      <div>
         <input type="password" placeholder="비밀번호를 입력해주세요" />
       </div>
       <div>
         <button>로그인</button>
-      </div>
-      <div>
         <button>회원가입</button>
       </div>
-    </>
+    </Logo>
   );
 };
 
 export default HomeLoginPage;
+
+export const Logo = styled.div`
+  align-items: center;
+  text-align: center;
+  margin: 25% auto;
+`;
