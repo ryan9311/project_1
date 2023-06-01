4b414b11993955d4f4941ee0ad131ac4f8b4ed3e
h-jeong0213 <rhdwn3571@naver.com>
2023-06-01 12:45:11 +0900

    signUp html,css추가

--- foodmap/src/pages/signUp/index.js
+++ foodmap/src/pages/signUp/index.js
@@ -1,9 +1,62 @@
+import styled from '@emotion/styled'
+import Image from 'next/image'
+import { Logo } from '..'
+
 const signupPage = () => {
   return (
-    <>
-      <h1>회원가입 페이지</h1>
-    </>
-  );
-};
+    <SignupWrap>
+      <div>
+        <Logo></Logo>
+      </div>
+      <div>
+        <InputWrap>
+          <Input type="text" placeholder="ID를 입력해주세요" />
+        </InputWrap>
+        <div>
+          <Input type="text" placeholder="닉네임을 입력해주세요" />
+        </div>
+        <div>
+          <Input type="password" placeholder="비밀번호" />
+        </div>
+        <div>
+          <Input type="password" placeholder="비밀번호 확인" />
+        </div>
+        <Button>가입하기</Button>
+      </div>
+    </SignupWrap>
+  )
+}
 
-export default signupPage;
+export default signupPage
+export const SignupWrap = styled.div`
+  //display: flex;
+  justify-content: center;
+  align-items: center;
+  //height: 100vh;
+  margin-top: 20vh;
+  text-align: center;
+`
+export const InputWrap = styled.div`
+  // height: 20px;
+  margin: 5px;
+`
+export const Input = styled.input`
+  align-items: center;
+  //width: 20%;
+  margin: 3px;
+  font-size: 20px;
+  text-align: center;
+  width: 400px;
+  height: 30px;
+`
+
+export const Button = styled.button`
+  width: 150px;
+  height: 25px;
+  // border-radius: 15px;
+  align-items: center;
+  border: 1px solid gray;
+  //background-color: pink;
+  font-size: 15px;
+  font-weight: 30px;
+`
