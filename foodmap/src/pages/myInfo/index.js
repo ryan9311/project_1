6cf69fd1952b09c890c4f32b28bdd93f7de689b5
YongKi <p.ryan9311@gmail.com>
2023-06-04 17:03:03 +0900

    구글 지도 추가

--- foodmap/src/pages/myInfo/index.js
+++ foodmap/src/pages/myInfo/index.js
@@ -2,17 +2,27 @@ import FoodCard from "@/components/common/foodCard";
 import HeaderNav from "@/components/common/header_nav";
 import Layout from "@/components/common/layout";
 import styled from "@emotion/styled";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 const MyinfoPage = () => {
   return (
     <>
       <Layout></Layout>
-      <MyMap>지도</MyMap>
+      <LoadScript
+        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+      >
+        <MyMap>
+          <GoogleMap
+            mapContainerStyle={{ height: "100%", width: "100%" }}
+            zoom={10}
+            center={{ lat: 37.498095, lng: 127.02761 }} // 지도 초기 좌표
+          />
+        </MyMap>
+      </LoadScript>
       <FoodCard></FoodCard>
     </>
   );
 };
-
 export default MyinfoPage;
 
 export const MyMap = styled.div`
