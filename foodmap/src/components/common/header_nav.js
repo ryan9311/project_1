6172b313c0ae902e00064a7192e4431330d5f2ef
YongKi <p.ryan9311@gmail.com>
2023-06-04 11:10:30 +0900

    햄버거 완성!

--- foodmap/src/components/common/header_nav.js
+++ foodmap/src/components/common/header_nav.js
@@ -1,21 +1,33 @@
-import styled from '@emotion/styled'
+import styled from "@emotion/styled";
+import { useState } from "react";
 
 const HeaderNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
       <Header>
         <MiniLogo></MiniLogo>
-        <HamNav>
+        <HamNav onClick={toggleMenu} isOpen={isOpen}>
           <HamNavSpan></HamNavSpan>
           <HamNavSpan></HamNavSpan>
           <HamNavSpan></HamNavSpan>
         </HamNav>
+        {isOpen && (
+          <Menu>
+            <Menus>홈</Menus>
+            <Menus>내 맛집</Menus>
+            <Menus>리뷰 관리</Menus>
+          </Menu>
+        )}
       </Header>
     </>
-  )
-}
+  );
+};
 
-export default HeaderNav
+export default HeaderNav;
 
 export const Header = styled.div`
   width: 1000px;
@@ -23,7 +35,7 @@ export const Header = styled.div`
   justify-content: space-between;
   align-items: center;
   margin: 0 auto;
-`
+`;
 
 export const MiniLogo = styled.div`
   border: 1px solid black;
@@ -32,7 +44,7 @@ export const MiniLogo = styled.div`
   background-image: url(/Logo.png);
   background-size: cover;
   background-position: center;
-`
+`;
 
 export const HamNav = styled.a`
   display: flex;
@@ -40,11 +52,27 @@ export const HamNav = styled.a`
   flex-wrap: wrap;
   width: 50px;
   height: 50px;
-`
+`;
 
 export const HamNavSpan = styled.span`
   height: 4px;
   background-color: blue;
   width: 100%;
   border-radius: 10px;
-`
+`;
+
+export const Menu = styled.div`
+  background-color: white;
+  position: absolute;
+  z-index: 3;
+  top: 100px;
+  right: 140px;
+  width: 200px;
+  padding: 10px;
+  border: 1px solid black;
+`;
+
+export const Menus = styled.a`
+  display: block;
+  margin-bottom: 10px;
+`;
