612ed57da4d27c4c4be4f8a97da963a1ca16d4c0
YongKi <p.ryan9311@gmail.com>
2023-06-04 17:36:39 +0900

    디자인 수정

--- foodmap/src/pages/index.js
+++ foodmap/src/pages/index.js
@@ -10,7 +10,7 @@ const HomeLoginPage = () => {
           <InputBox type="text" name="id" placeholder="ID를 입력해주세요" />
           <InputBox type="password" name="pw" placeholder="PW를 입력해주세요" />
           <LoginPageBtn>로그인</LoginPageBtn>
-          <LoginPageBtn>회원가입</LoginPageBtn>
+          <SignUp>SignUp</SignUp>
         </Login>
       </BodyContainer>
     </>
@@ -55,14 +55,38 @@ export const InputBox = styled.input`
   margin-bottom: 20px;
   width: 400px;
   height: 30px;
-  font-size: 20px;
+  font-size: 1rem;
+  background: rgba(250, 250, 250);
+  text-align: left;
+  padding: 9px 0 7px 8px;
+  border: 0.1px solid gray;
+  border-radius: 5px;
 `;
 
 export const LoginPageBtn = styled.button`
   width: 200px;
-  height: 30px;
-  background-color: #fff7f7;
-  align-items: center;
-  text-align: center;
+  height: 40px;
+  background-color: #3a8ef0;
+  border-radius: 15px;
   margin-bottom: 20px;
+  font-weight: bold;
+  font-size: 20px;
+  color: whitesmoke;
+  border: none;
+  cursor: pointer;
+`;
+
+export const SignUp = styled.a`
+  width: 200px;
+  height: 30px;
+  background-color: #3a8ef0;
+  border-radius: 15px;
+  margin-bottom: 20px;
+  font-weight: bold;
+  font-size: 20px;
+  color: whitesmoke;
+  border: none;
+  margin-bottom: 20px;
+  border: 2px solid black;
+  cursor: pointer;
 `;
